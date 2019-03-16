66bc6b68cd85a062c631b4d8c87cfa0dea4b8946
Rizwan <rizwankhan.august16@gmail.com>
2019-03-16 23:33:55 +0530

    index.php page session update

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,12 @@
-<?php session_start();  include "./templates/top.php"; ?>
+<?php 
+session_start();  
+if (!isset($_SESSION['admin_id'])) {
+  header("location:login.php");
+}
+
+include "./templates/top.php"; 
+
+?>
  
 <?php include "./templates/navbar.php"; ?>
 
